19ee2038020efb0a447b693b8caf328f85eafaec
gorushkin <gorushkin@users.noreply.github.com>
2018-07-19 23:09:24 +0300

    Попапы стилизованы почти

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ var popUp = document.querySelector(".write-to-us");
 var closeButton = document.querySelector(".write-to-us-close");
 var map = document.querySelector(".map-fog");
 var popUpMap = document.querySelector(".map-popUp");
+var closeMap = document.querySelector(".map-close");
 
 
 link.addEventListener("click", function (evt) {
@@ -25,26 +26,12 @@ map.addEventListener("click", function (evt) {
     popUpMap.classList.add("modal-show");
 })
 
-// if (popUpMap.classList.contains("modal-show")) {
-//     console.log('yeess');
-// }
-
-// console.log(popUpMap.classList.contains("modal-show"));
-
-// var all = document.body;
-
-// all.addEventListener("wheel", function (evt) {
-//     evt.preventDefault();
-//     console.log('крутится!!!');
-// })
-
-// all.addEventListener("click", function (e) {
-//     // e.preventDefault();
-//     // console.log("кликаем");
-//     if (popUp.classList.contains("modal-show")) {
-//         console.log('пора уходить');
-//     }
-// })
+closeMap.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  console.log("closing");
+  popUpMap.classList.remove("modal-show");
+}
+)
 
 window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
