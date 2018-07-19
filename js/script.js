d5b069b2934526ef64a85825c94e7b47337dd3e6
gorushkin <gorushkin@gmail.com>
2018-07-19 16:15:35 +0300

    приделал реакцию на esc

--- js/script.js
+++ js/script.js
@@ -25,3 +25,37 @@ map.addEventListener("click", function (evt) {
     popUpMap.classList.add("modal-show");
 })
 
+// if (popUpMap.classList.contains("modal-show")) {
+//     console.log('yeess');
+// }
+
+// console.log(popUpMap.classList.contains("modal-show"));
+
+// var all = document.body;
+
+// all.addEventListener("wheel", function (evt) {
+//     evt.preventDefault();
+//     console.log('крутится!!!');
+// })
+
+// all.addEventListener("click", function (e) {
+//     // e.preventDefault();
+//     // console.log("кликаем");
+//     if (popUp.classList.contains("modal-show")) {
+//         console.log('пора уходить');
+//     }
+// })
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+
+      if (popUp.classList.contains("modal-show")) {
+        popUp.classList.remove("modal-show");
+      }
+
+      if (popUpMap.classList.contains("modal-show")) {
+        popUpMap.classList.remove("modal-show");
+      }
+    }
+  })
