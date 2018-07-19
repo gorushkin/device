bad50f90874176a56f7813dbe130521fd888e56f
gorushkin <gorushkin@gmail.com>
2018-07-19 15:35:37 +0300

    Оживил немного карту

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 var link = document.querySelector(".write-to-us-link");
 var popUp = document.querySelector(".write-to-us");
 var closeButton = document.querySelector(".write-to-us-close");
+var map = document.querySelector(".map-fog");
+var popUpMap = document.querySelector(".map-popUp");
+
 
 link.addEventListener("click", function (evt) {
     evt.preventDefault();
@@ -15,3 +18,10 @@ closeButton.addEventListener("click", function (evt) {
     popUp.classList.remove("modal-show");
 }
 )
+
+map.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    console.log('open map, please');
+    popUpMap.classList.add("modal-show");
+})
+
